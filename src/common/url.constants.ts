dce3a45b15e1290696e47198e0527ab94cffd22e
goran9999 <gutovicgoran@gmail.com>
2023-01-15 16:58:32 +0100

    added main cards on home page

---
+++ src/common/url.constants.ts
@@ -0,0 +1,4 @@
+export const HOME_PAGE = "/";
+export const PROGRAMS = "/programs";
+export const NFTs = "/nfts";
+export const FUNGIBLE_TOKENS = "/fungible-tokens";
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import { NFTs, PROGRAMS } from "../common/url.constants";
+import CardInfo from "../components/Accounts/CardInfo/CardInfo";
+import "./HomePage.scss";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="home-page">
+      <div className="home-page__cards">
+        <CardInfo
+          amount={11}
+          title="Your loaded programs"
+          showButton
+          navigateTo={PROGRAMS}
+        />
+        <CardInfo
+          amount={12}
+          title="Transactions count"
+          description="Transactions per second (TPS)"
+          secondAmount={2}
+        />
+        <CardInfo
+          amount={11}
+          title="Total NFTs minted"
+          showButton
+          navigateTo={NFTs}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
