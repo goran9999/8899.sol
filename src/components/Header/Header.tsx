4d0f6bef721ac0c3e7af78ea3e122887bd54d016
goran9999 <gutovicgoran@gmail.com>
2023-01-22 22:19:37 +0100

    updated favicon and additional fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import React from "react";
+import { useNavigate } from "react-router";
+import logo from "../../assets/logo.png";
+import { PROGRAMS } from "../../common/url.constants";
 import "./Header.scss";
 const Header = () => {
   const wallet = useWallet();
 
+  const navigate = useNavigate();
+
   return (
     <div className="header">
-      <h1>LOGO</h1>
+      <img onClick={() => navigate("/")} src={logo} alt="logo" />
       <div className="header__navbar">
         <ul>
-          <li>Programs</li>
+          <li onClick={() => navigate(PROGRAMS)}>Programs</li>
           <a title="Comming soon">NFTs</a>
           <a title="Comming soon">Fungible tokens</a>
           <li>
